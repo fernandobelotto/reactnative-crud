b1e388758c2879f9ef81060b57ad6c3ffab5d30e
Fernando Belotto <fernando.bbosco@gmail.com>
2021-02-22 21:35:07 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text style={styles.highlight}>App.js</Text>
+    </>
+  );
+};
+
+
+export default App;
